aee56c47b6cedcdf9b4da02e23d4e3918e99b3b7
Victor <kingpin11@infrawareglobal.com>
2017-10-19 19:15:09 +0900

    Create Project

---
+++ MVVM-Swift/MVVM-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM-Swift
+//
+//  Created by victor on 2017. 10. 19..
+//  Copyright © 2017년 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
