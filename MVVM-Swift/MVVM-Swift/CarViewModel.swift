9f055d226a61e2b462d678c0d86f39ad96c00de2
Victor <kingpin11@infrawareglobal.com>
2017-10-19 19:41:02 +0900

    Add some decoration to pass the xctest

--- MVVM-Swift/MVVM-Swift/CarViewModel.swift
+++ MVVM-Swift/MVVM-Swift/CarViewModel.swift
@@ -8,13 +8,33 @@
 
 import Foundation
 
+//CarViewModel add a bit of decoration to our plain class.
 class CarViewModel {
     private var car: Car?
-    var modelText: String?
-    var makeText: String?
-    var horsepowerText: String?
-    var titleText: String?
-    var photoURL: NSURL?
+    var modelText: String? {
+        return car?.model
+    }
+    var makeText: String? {
+        return car?.make
+    }
+    var horsepowerText: String? {
+        guard let horsepower = car?.horsepower else {
+            return nil
+        }
+        return "\(horsepower) HP"
+    }
+    var titleText: String? {
+        guard let make = car?.make, let model = car?.model else {
+            return nil
+        }
+        return "\(make) \(model)"
+    }
+    var photoURL: NSURL? {
+        guard let photoURL = car?.photoURL else {
+            return nil
+        }
+        return NSURL(string: photoURL)
+    }
     
     init(car: Car) {
         self.car = car
